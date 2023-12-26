fd98b6ec206ab8ce2999514e020d2155b3d1d9a2
희승 <heeseung@huiseung-ui-MacBookPro.local>
2023-12-26 19:52:05 +0900

    NCP Service CRUD 기능 Reconcile func 연동

--- api/v1/provision_types.go
+++ api/v1/provision_types.go
@@ -20,22 +20,50 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
-// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
-
 // ProvisionSpec defines the desired state of Provision
 type ProvisionSpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
-
-	// Foo is an example field of Provision. Edit provision_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
+	AccessControlGroupNoListN                     string `json:"accessControlGroupNoList,omitempty"`
+	AssociateWithPublicIp                         bool   `json:"associateWithPublicIp,omitempty"`
+	BlockDevicePartitionMountPoint                string `json:"blockDevicePartitionMountPoint,omitempty"`
+	BlockDevicePartitionSize                      string `json:"blockDevicePartitionSize,omitempty"`
+	BlockStorageMappingBlockStorageName           string `json:"blockStorageMappingBlockStorageName,omitempty"`
+	BlockStorageMappingBlockStorageSize           string `json:"blockStorageMappingBlockStorageSize,omitempty"`
+	BlockStorageMappingBlockStorageVolumeTypeCode string `json:"blockStorageMappingBlockStorageVolumeTypeCode,omitempty"`
+	BlockStorageMappingEncrypted                  string `json:"blockStorageMappingEncrypted,omitempty"`
+	BlockStorageMappingOrder                      int    `json:"blockStorageMappingList,omitempty"`
+	BlockStorageMappingSnapshotInstanceNo         string `json:"blockStorageMappingSnapshotInstanceNo,omitempty"`
+	FeeSystemTypeCode                             string `json:"feeSystemTypeCode,omitempty"`
+	InitScriptNo                                  string `json:"initScriptNo,omitempty"`
+	IsEncryptedBaseBlockStorageVolume             bool   `json:"isEncryptedBaseBlockStorageVolume,omitempty"`
+	IsProtectServerTermination                    bool   `json:"isProtectServerTermination,omitempty"`
+	LoginKeyName                                  string `json:"loginKeyName,omitempty"`
+	MemberServerImageInstanceNo                   string `json:"memberServerImageInstanceNo,omitempty"`
+	NetworkInterfaceIp                            string `json:"networkInterfaceIp,omitempty"`
+	NetworkInterfaceNo                            string `json:"networkInterfaceNo,omitempty"`
+	NetworkInterfaceOrder                         int    `json:"networkInterfaceList,omitempty"`
+	NetworkInterfaceSubnetNo                      string `json:"networkInterfaceSubnetNo,omitempty"`
+	PlacementGroupNo                              string `json:"placementGroupNo,omitempty"`
+	Verb                                          string `json:"verb,omitempty"`
+	RAIDTypeName                                  string `json:"raidTypeName,omitempty"`
+	ResponseFormatType                            string `json:"responseFormatType,omitempty"`
+	ServerCreateCount                             int    `json:"serverCreateCount,omitempty"`
+	ServerCreateStartNo                           int    `json:"serverCreateStartNo,omitempty"`
+	ServerDescription                             string `json:"serverDescription,omitempty"`
+	ServerImageNo                                 string `json:"serverImageNo,omitempty"`
+	ServerImageProductCode                        string `json:"serverImageProductCode,omitempty"`
+	ServerName                                    string `json:"serverName,omitempty"`
+	ServerProductCode                             string `json:"serverProductCode,omitempty"`
+	ServerSpecCode                                string `json:"serverSpecCode,omitempty"`
+	SubnetNo                                      string `json:"subnetNo,omitempty"`
+	VpcNo                                         string `json:"vpcNo,omitempty"`
 }
 
 // ProvisionStatus defines the observed state of Provision
 type ProvisionStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
+	Version       int    `json:"version,omitempty"`
+	Errors        int    `json:"errors,omitempty"`
+	Warnings      int    `json:"warnings,omitempty"`
+	FailureReason string `json:"failureReason,omitempty"`
 }
 
 //+kubebuilder:object:root=true
---
+++ internal/controller/constants.go
@@ -0,0 +1,15 @@
+package controller
+
+const (
+	apiUrlCreate        = "https://ncloud.apigw.ntruss.com/vserver/v2/createServerInstances"
+	apiUrlDelete        = "https://ncloud.apigw.ntruss.com/vserver/v2/terminateServerInstances"
+	apiUrlGet           = "https://ncloud.apigw.ntruss.com/vserver/v2/getMemberServerImageInstanceList"
+	apiUrlStop          = "https://ncloud.apigw.ntruss.com/vserver/v2/stopServerInstances"
+	apiUrlUpdate        = "https://ncloud.apigw.ntruss.com/vserver/v2/changeServerInstanceSpec"
+	ProvisionVerbCreate = "Create"
+	ProvisionVerbDelete = "Delete"
+	ProvisionVerbGet    = "Get"
+	ProvisionVerbList   = "List"
+	ProvisionVerbStop   = "Stop"
+	ProvisionVerbUpdate = "Update"
+)
